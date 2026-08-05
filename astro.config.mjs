cbf616097d62ca3b889d7e9c4b0096663835aaf6
RiceWa <jamesfitze007@gmail.com>
2026-08-05 16:56:02 -0400

    Set up for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   // site and base is currently set for github pages, update this once we get a domain.
-  site: 'https/RiceWa.github.io',
+  site: 'https://RiceWa.github.io',
   base: '/SnapFormWeb',
   vite: {
     plugins: [tailwindcss()]
